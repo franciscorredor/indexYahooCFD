91e4666403f03022b24bdc30392b4745a6e2191f
Francisco <francisco.corredor.pardo@gmail.com>
2015-11-28 07:37:44 -0500

    act

--- indicesToCFD/src/com/wireless/soft/indices/cfd/deserializable/json/object/ReturnIndexYahooFinanceObject.java
+++ indicesToCFD/src/com/wireless/soft/indices/cfd/deserializable/json/object/ReturnIndexYahooFinanceObject.java
@@ -173,6 +173,74 @@ public class ReturnIndexYahooFinanceObject implements Serializable {
 			
 			public class Fields{
 				
+				private String name;
+				private String price;
+				private String symbol;
+				private String ts;
+				private String type;
+				private String utctime;
+				private String volume;
+				
+				public String getName() {
+					return name;
+				}
+				public void setName(String name) {
+					this.name = name;
+				}
+				public String getPrice() {
+					return price;
+				}
+				public void setPrice(String price) {
+					this.price = price;
+				}
+				public String getSymbol() {
+					return symbol;
+				}
+				public void setSymbol(String symbol) {
+					this.symbol = symbol;
+				}
+				public String getTs() {
+					return ts;
+				}
+				public void setTs(String ts) {
+					this.ts = ts;
+				}
+				public String getType() {
+					return type;
+				}
+				public void setType(String type) {
+					this.type = type;
+				}
+				public String getUtctime() {
+					return utctime;
+				}
+				public void setUtctime(String utctime) {
+					this.utctime = utctime;
+				}
+				public String getVolume() {
+					return volume;
+				}
+				public void setVolume(String volume) {
+					this.volume = volume;
+				}
+				
+				@Override
+			    public String toString() {
+				StringBuffer s = new StringBuffer();
+				s.append("\n name [" + this.name + "]");
+				s.append("\n price [" + this.price + "]");
+				s.append("\n symbol [" + this.symbol + "]");
+				
+				s.append("\n ts [" + this.ts + "]");
+				s.append("\n type [" + this.type + "]");
+				s.append("\n utctime [" + this.utctime + "]");
+				
+				s.append("\n volume [" + this.volume + "]");
+				
+				return s.toString();
+			    }	
+				
+				
 			}
 			
 			@Override
